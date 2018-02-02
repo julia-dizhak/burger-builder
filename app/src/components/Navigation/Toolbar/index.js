56516bde66879ed4185d47a83398e02bcf00c036
Yulia <yulia.scherbina@gmail.com>
2018-02-02 12:51:06 +0100

    using a logo in app

--- app/src/components/Navigation/Toolbar/index.js
+++ app/src/components/Navigation/Toolbar/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import classes from './Toolbar.css';
+import Logo from '../../Logo';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>Menu</div>
-        <div>Logo</div>
+        <Logo />
         <nav>
             ...
         </nav>
